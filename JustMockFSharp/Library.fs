a44ccca5d447e6fdd20b30773df2269af317dc7f
Franco Tiveron <franco@horthfinancial.com>
2023-04-12 15:39:42 +1000

    --

---
+++ JustMockFSharp/Library.fs
@@ -0,0 +1,7 @@
+﻿module internal M1
+    
+    module private M2 = 
+
+        let f1() = 1
+
+        let f2() = f1() * 2
---
+++ JustMockFSharpTests/Tests.fs
@@ -0,0 +1,9 @@
+module Tests
+
+open System
+open Xunit
+
+[<Fact>]
+let ``My test`` () =
+    // How Do I Mock/Call.. M1.M2.f1/f2?
+    ()
